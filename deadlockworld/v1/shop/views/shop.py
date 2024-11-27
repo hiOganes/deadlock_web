7ae8bf971a7949ac159193d067164e45357b8cd1
Oganes <123650676+hiOganes@users.noreply.github.com>
2024-11-27 16:58:18 +0300

    add search

--- deadlockworld/v1/shop/views/shop.py
+++ deadlockworld/v1/shop/views/shop.py
@@ -1,6 +1,6 @@
 # Standart libraries
 # Other libraries
-from rest_framework import generics
+from rest_framework import generics, filters
 
 # Project libraries
 from v1.shop.models.shop import Shop
@@ -9,6 +9,8 @@ from v1.shop.serializers.shop import ShopSerializer
 
 class ShopView(generics.ListAPIView):
     serializer_class = ShopSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
     
     def get_queryset(self):
         return Shop.objects.filter(category=self.kwargs['category'])
